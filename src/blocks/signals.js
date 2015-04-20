12c5da5631eeb0d92a2950301699ad72f946dda4
Samuele Mattiuzzo <samumatt@gmail.com>
2015-04-20 02:38:25 +0100

    Signals

---
+++ src/blocks/signals.js
@@ -0,0 +1,23 @@
+/*
+    signals = {}
+    
+    read = 
+    <ID (int)> : {
+        'text': <string>,
+        'sadness': <int>,
+        'hope': <int>,
+        'wisdom': <int>,
+        'confusion': <int>,
+        'signal': <ID (int)> // increments or lowers the signal strength
+        },
+        'link_to': <ID (int)> // read_id
+    }
+*/
+
+SIGNALS = {
+    1: {
+        'text': {
+
+        }
+    }
+};
